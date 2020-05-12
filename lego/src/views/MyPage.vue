c4f6e538d207351bb2acb979811748e5e033b7d4
김정덕 <ijung1@naver.com>
2020-05-12 18:03:06 +0900

    front/mypage

--- lego/src/views/MyPage.vue
+++ lego/src/views/MyPage.vue
@@ -5,7 +5,12 @@
         <MypageHeader></MypageHeader>
       </div>
       <hr />
-      <div class="body_menu_bar"></div>
+      <div class="body_menu_bar">
+        <button class="menu" v-for="i in menus.length" :key="i">
+          <i :class="menuIcons[i-1]"></i>
+          {{menus[i-1]}}
+        </button>
+      </div>
       <div class="body">
         <MypageBody></MypageBody>
       </div>
@@ -22,7 +27,16 @@ export default {
     MypageBody
   },
   data() {
-    return {};
+    return {
+      menus: ["설계도", "부품", "좋아요", "기타", "등등"],
+      menuIcons: [
+        "fas fa-scroll",
+        "fas fa-cubes",
+        "fas fa-heart",
+        "fas fa-heart",
+        "fas fa-heart"
+      ]
+    };
   }
 };
 </script>
@@ -43,15 +57,29 @@ export default {
   margin-bottom: 20px;
 }
 hr {
-  border: rgb(252, 193, 85) solid 2px;
+  border: rgb(252, 193, 85) solid 1.2px;
 }
 .body_menu_bar {
-  border: black solid 1px;
+  display: flex;
+  border-style: none;
   height: 50px;
   margin-bottom: 20px;
+  margin-top: 10px;
+  justify-content: center;
+}
+.menu {
+  font-size: 17px;
+  color: rgb(160, 159, 159);
+  margin-right: 40px;
+  width: 6vw;
+}
+.menu:hover {
+  color: black;
+  font-size: 18px;
+  font: x-large;
 }
 .body {
-  border: black solid 1px;
+  border-style: none;
   height: 100%;
   width: 100%;
 }
