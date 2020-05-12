eece5ea1de3b2290e06210459778e9babd789282
이지훈 <hopehappy92@naver.com>
2020-05-12 11:06:40 +0900

    [지훈] install vuetify

---
+++ lego/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app>
+    <route-view />
+    <go-top />
+  </v-app>
+</template>
+
+<script>
+import RouteView from "@/components/RouteView";
+import GoTop from "@/components/GoTop";
+
+export default {
+  name: "App",
+
+  components: {
+    RouteView,
+    GoTop
+  },
+
+  data: () => ({
+    //
+  })
+};
+</script>
---
+++ lego/src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    Home
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "Home",
+  components: {}
+};
+</script>
