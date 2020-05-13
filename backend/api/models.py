978a6f82eaaa7b742c073042d4940a6e7d6dda43
윤명훈 <audgnsdhvkf@naver.com>
2020-05-13 15:51:51 +0900

    db 모델링 수정

--- backend/api/models.py
+++ backend/api/models.py
@@ -3,83 +3,86 @@ from django.contrib.auth.models import AbstractUser
 from django.conf import settings
 
 class CustomUser(AbstractUser):
-    nickname = models.CharField(max_length=50)
-    user_image = models.TextField(null=True)
+    nickName = models.CharField(max_length=50)
+    image = models.TextField(null=True)
     comment = models.CharField(max_length=100, null=True)
     age = models.IntegerField(null=True)
     gender = models.IntegerField(null=True)
-    user_parts = models.TextField(null=True)
+    parts = models.TextField(null=True)
     followers = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='followings')
 
-class Lego_set(models.Model):
+class LegoSet(models.Model):
     id = models.IntegerField(primary_key=True)
-    user_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    set_name = models.CharField(max_length=100, null=True)
-    set_parts = models.TextField(null=True)
-    set_image = models.TextField(null=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, null=True)
+    parts = models.TextField(null=True)
+    images = models.TextField(null=True)
     description = models.CharField(max_length=500, null=True)
     tags = models.CharField(max_length=200, null=True)
     references = models.CharField(max_length=500, null=True)
-    like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="like_sets", blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    likeUsers = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="likeSets", blank=True)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_now=True)
 
     @property
-    def tag_list(self):
+    def tagList(self):
         return self.tags.split("|") if self.tags else []
     @property
-    def reference_list(self):
+    def referenceList(self):
         return self.references.split("|") if self.references else []
+    @property
+    def imageList(self):
+        return self.images.split("|") if self.images else []
 
     def __str__(self):
-        return self.set_name
+        return self.name
 
 class Review(models.Model):
     id = models.IntegerField(primary_key=True)
-    set_id = models.ForeignKey(Lego_set, on_delete=models.CASCADE)
-    user_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    setId = models.ForeignKey(LegoSet, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     content = models.TextField()
     score = models.IntegerField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.content
 
-class Lego_part(models.Model):
+class LegoPart(models.Model):
     id = models.IntegerField(primary_key=True)
-    part_name = models.CharField(max_length=100, null=True)
-    part_image = models.TextField(null=True)
-    bricklink_ids = models.TextField(null=True)
+    name = models.CharField(max_length=100, null=True)
+    image = models.TextField(null=True)
+    bricklinkIds = models.TextField(null=True)
 
     @property
-    def bricklink_ids_list(self):
-        return self.bricklink_ids.split("|") if self.bricklink_ids else []
+    def bricklinkIdList(self):
+        return self.bricklinkIds.split("|") if self.bricklinkIds else []
 
     def __str__(self):
-        return self.part_name
+        return self.name
 
 class Color(models.Model):
     id = models.IntegerField(primary_key=True)
-    color_name = models.CharField(max_length=100, null=True)
-    color_rgb = models.TextField(null=True)
-    bricklink_ids = models.TextField(null=True)
-    bricklink_descrs = models.TextField(null=True)
-    official_ids = models.TextField(null=True)
-    official_descrs = models.TextField(null=True)
+    name = models.CharField(max_length=100, null=True)
+    rgb = models.TextField(null=True)
+    bricklinkIds = models.TextField(null=True)
+    bricklinkDescrs = models.TextField(null=True)
+    officialIds = models.TextField(null=True)
+    officialDescrs = models.TextField(null=True)
 
     @property
-    def bricklink_ids_list(self):
-        return self.bricklink_ids.split("|") if self.bricklink_ids else []
+    def bricklinkIdList(self):
+        return self.bricklinkIds.split("|") if self.bricklinkIds else []
     @property
-    def bricklink_descrs_list(self):
-        return self.bricklink_descrs.split("|") if self.bricklink_descrs else []
+    def bricklinkDescrsList(self):
+        return self.bricklinkDescrs.split("|") if self.bricklinkDescrs else []
     @property
-    def official_ids_list(self):
-        return self.official_ids.split("|") if self.official_ids else []
+    def officialIdList(self):
+        return self.officialIds.split("|") if self.officialIds else []
     @property
-    def official_descrs_list(self):
-        return self.official_descrs.split("|") if self.official_descrs else []
+    def officialDescrsList(self):
+        return self.officialDescrs.split("|") if self.officialDescrs else []
 
     def __str__(self):
-        return self.color_name
+        return self.name
