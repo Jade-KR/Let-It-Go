ceb2962144282bcfbd6a7e15a3a4a148b45d4afb
윤명훈 <audgnsdhvkf@naver.com>
2020-05-13 16:07:25 +0900

    db 모델링 수정

--- backend/api/models.py
+++ backend/api/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import AbstractUser
 from django.conf import settings
 
 class CustomUser(AbstractUser):
-    nickName = models.CharField(max_length=50)
+    nick_name = models.CharField(max_length=50)
     image = models.TextField(null=True)
     comment = models.CharField(max_length=100, null=True)
     age = models.IntegerField(null=True)
@@ -20,9 +20,9 @@ class LegoSet(models.Model):
     description = models.CharField(max_length=500, null=True)
     tags = models.CharField(max_length=200, null=True)
     references = models.CharField(max_length=500, null=True)
-    likeUsers = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="likeSets", blank=True)
-    createdAt = models.DateTimeField(auto_now_add=True)
-    updatedAt = models.DateTimeField(auto_now=True)
+    like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="like_sets", blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     @property
     def tagList(self):
@@ -39,12 +39,12 @@ class LegoSet(models.Model):
 
 class Review(models.Model):
     id = models.IntegerField(primary_key=True)
-    setId = models.ForeignKey(LegoSet, on_delete=models.CASCADE)
+    set_id = models.ForeignKey(LegoSet, on_delete=models.CASCADE)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     content = models.TextField()
     score = models.IntegerField()
-    createdAt = models.DateTimeField(auto_now_add=True)
-    updatedAt = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.content
@@ -53,11 +53,11 @@ class LegoPart(models.Model):
     id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=100, null=True)
     image = models.TextField(null=True)
-    bricklinkIds = models.TextField(null=True)
+    bricklink_ids = models.TextField(null=True)
 
     @property
     def bricklinkIdList(self):
-        return self.bricklinkIds.split("|") if self.bricklinkIds else []
+        return self.bricklink_ids.split("|") if self.bricklink_ids else []
 
     def __str__(self):
         return self.name
@@ -66,23 +66,23 @@ class Color(models.Model):
     id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=100, null=True)
     rgb = models.TextField(null=True)
-    bricklinkIds = models.TextField(null=True)
-    bricklinkDescrs = models.TextField(null=True)
-    officialIds = models.TextField(null=True)
-    officialDescrs = models.TextField(null=True)
+    bricklink_ids = models.TextField(null=True)
+    bricklink_descrs = models.TextField(null=True)
+    official_ids = models.TextField(null=True)
+    official_descrs = models.TextField(null=True)
 
     @property
     def bricklinkIdList(self):
-        return self.bricklinkIds.split("|") if self.bricklinkIds else []
+        return self.bricklink_ids.split("|") if self.bricklink_ids else []
     @property
     def bricklinkDescrsList(self):
-        return self.bricklinkDescrs.split("|") if self.bricklinkDescrs else []
+        return self.bricklink_descrs.split("|") if self.bricklink_descrs else []
     @property
     def officialIdList(self):
-        return self.officialIds.split("|") if self.officialIds else []
+        return self.official_ids.split("|") if self.official_ids else []
     @property
     def officialDescrsList(self):
-        return self.officialDescrs.split("|") if self.officialDescrs else []
+        return self.official_descrs.split("|") if self.official_descrs else []
 
     def __str__(self):
         return self.name
