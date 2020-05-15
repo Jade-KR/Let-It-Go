5accc49e91a1232c380e1ea5c8ba5b6a151c6503
윤명훈 <audgnsdhvkf@naver.com>
2020-05-15 13:37:01 +0900

    rest framework, swagger 적용 S02P31D108-31 S02P31D108-37 S02P31D108-39 S02P31D108-46

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework_swagger',
+    "drf_yasg",
+
 ]
 
 MIDDLEWARE = [
@@ -119,5 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
 
 AUTH_USER_MODEL = 'api.CustomUser'
