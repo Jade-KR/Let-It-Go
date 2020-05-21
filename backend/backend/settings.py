efd788499727363dd891020934913c32b68d419f
김평강 <pyeonggangkim@gmail.com>
2020-05-21 23:03:03 +0900

    static 설정 중

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -170,8 +170,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_DIRS = (
-    os.path.normpath(os.path.join(BASE_DIR, 'static')),
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
 )
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
