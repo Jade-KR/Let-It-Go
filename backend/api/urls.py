1398d964feb04a62e1a9f1729b1d4247a0bdb617
김평강 <pyeonggangkim@gmail.com>
2020-05-21 22:50:37 +0900

    static 설정 중

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -5,7 +5,7 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework_jwt.views import obtain_jwt_token, verify_jwt_token, refresh_jwt_token
 from django.contrib.auth.views import PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
-
+from django.conf.urls.static import static
 
 schema_view = get_schema_view(
     openapi.Info(
